c87047fda2165c8b00d36d150d5fc233f9761f62
daudujohn <daudujoe@gmail.com>
2023-03-07 10:32:32 +0100

    Updated settings.py

--- bincom_project/settings.py
+++ bincom_project/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'bincom_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bincomdb',
+        'USER': 'root',
+        'PASSWORD': 'universal0.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
